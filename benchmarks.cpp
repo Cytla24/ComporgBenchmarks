7acc2dbe66780679ba44fbbcc0e8a5b595ad2650
Cytla24 <ojuba2013@gmail.com>
2020-02-04 21:57:47 -0500

    first commit

---
+++ benchmarks.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : benchmarks.cpp
+// Author      : Aimie Ojuba
+// Description : Benchmarks
+//============================================================================
+
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+
+
+int main() {
+
+
+	return 0;
+}
