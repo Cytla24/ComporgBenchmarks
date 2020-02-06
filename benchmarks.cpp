2d130ee625781b42d2cdfa7452356e0c25607257
Cytla24 <ojuba2013@gmail.com>
2020-02-05 20:14:44 -0500

    wrote write array function

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <cmath>
 #include <ctime>
+#include <cstdint>
+
 using namespace std;
 
 void add(int a, int b);
@@ -15,8 +17,8 @@ void intdiv(int a, int b);
 void floatadd(float a, float b);
 void floatmult(float a, float b);
 void floatdiv(float a, float b);
-void readarr(int a[]);
-void writearr(int a[]);
+void readarr();
+void writearr();
 
 void add(int a = 3, int b = 4){
 	long long i = 0;
@@ -52,7 +54,7 @@ void floatadd(float a = 3.32, float b = 6.9){
 
 void floatmult(float a = 3.32, float b = 1.9){
 	long long i = 0;
-	while (i < 5000){
+	while (i < 50000000000){
 			a = a*b;
 			i++;
 		}
@@ -66,17 +68,23 @@ void floatdiv(float a = 3.32, float b = 6.9){
 		}
 };
 
-void readarr(int a[]){
+void readarr(){
 
 };
 
-void writarr(int a[]){
-
+void writarr(){
+	long long i = 0;
+	std::int32_t b = 2;
+	int *a = new int[5000000000];
+	while (i < 5000000000){
+		a[i] = b;
+		i++;
+	}
+	delete [] a;
 }
 
 int main() {
 	time_t aimie = time(NULL);
-	floatmult();
 	time_t newi = time(NULL);
 	cout << newi - aimie<< endl;
 
