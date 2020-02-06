98f025ad38a7db448d51b9ef1d3df153feeb3d55
Cytla24 <ojuba2013@gmail.com>
2020-02-05 19:19:30 -0500

    declared read and write array functions

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -15,6 +15,8 @@ void intdiv(int a, int b);
 void floatadd(float a, float b);
 void floatmult(float a, float b);
 void floatdiv(float a, float b);
+void readarr(int a[]);
+void writearr(int a[]);
 
 void add(int a = 3, int b = 4){
 	long long i = 0;
@@ -25,7 +27,7 @@ void add(int a = 3, int b = 4){
 }
 
 void intmult(int a=4, int b=5){
-	long long i = 0;\
+	long long i = 0;
 	while (i < 50000000000){
 			a*=b;
 			i++;
@@ -33,40 +35,48 @@ void intmult(int a=4, int b=5){
 };
 
 void intdiv(int a=4, int b=5){
-	long long i = 0;\
+	long long i = 0;
 	while (i < 20000000000){
 			a/=b;
 			i++;
 		}
 };
 
-void floatadd(float a, float b){
-	long long i = 0;\
+void floatadd(float a = 3.32, float b = 6.9){
+	long long i = 0;
 	while (i < 100000000000){
 			a+=b;
 			i++;
 		}
 };
 
-void floatmult(float a, float b){
-	long long i = 0;\
-	while (i < 50000000000){
-			a*=b;
+void floatmult(float a = 3.32, float b = 1.9){
+	long long i = 0;
+	while (i < 5000){
+			a = a*b;
 			i++;
 		}
 };
 
-void floatdiv(float a, float b){
-	long long i = 0;\
+void floatdiv(float a = 3.32, float b = 6.9){
+	long long i = 0;
 	while (i < 20000000000){
 			a/=b;
 			i++;
 		}
 };
 
+void readarr(int a[]){
+
+};
+
+void writarr(int a[]){
+
+}
+
 int main() {
 	time_t aimie = time(NULL);
-	add();
+	floatmult();
 	time_t newi = time(NULL);
 	cout << newi - aimie<< endl;
 
